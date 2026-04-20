a76138ff5fc6b6165cd18a40c9a34c5d31c00f5b
Sevda Imany <SImany@SMLPSLURMMFT1.ign.fr>
2026-04-20 10:03:03 +0200

    clean code

--- main.py
+++ main.py
@@ -166,7 +166,10 @@ def main(cfg: DictConfig):
                 for prompt, neg_prompt in zip(prompts, neg_prompts):
                     logg.info(f"--- Processing: {img_name} | Prompt: '{prompt}' | Negative Prompt: '{neg_prompt}' ---")
                     img = load_image(img_name, cfg)
-                    generator.inference_change_style(img, img_name, prompt_seg, prompt, negative_prompt_inpaint=neg_prompt, save_all=True)
+                    generator.inference(img, img_name, prompt_seg, prompt, negative_prompt_inpaint=neg_prompt, save_all=True)
+            
+                    # if you want changing the weather
+                    # generator.inference_change_style(img, img_name, prompt_seg, prompt, negative_prompt_inpaint=neg_prompt, save_all=True)
             
             # Scenario B: Lengths differ (or one is length 1) -> Iterate on BOTH (all combinations)
             else:
@@ -174,7 +177,10 @@ def main(cfg: DictConfig):
                     for neg_prompt in neg_prompts:
                         logg.info(f"--- Processing: {img_name} | Prompt: '{prompt}' | Negative Prompt: '{neg_prompt}' ---")
                         img = load_image(img_name, cfg)
-                        generator.inference_change_style(img, img_name, prompt_seg, prompt, negative_prompt_inpaint=neg_prompt, save_all=True)   
+                        generator.inference(img, img_name, prompt_seg, prompt, negative_prompt_inpaint=neg_prompt, save_all=True)   
+
+                        # changing the weather
+                        # generator.inference_change_style(img, img_name, prompt_seg, prompt, negative_prompt_inpaint=neg_prompt, save_all=True)   
     else:
         logg.error(f"Unknown run_mode: {run_mode}")
 
